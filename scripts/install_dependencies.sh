e0e9aed93c8a2142b2baf49f30b6fa751a0609ee
soumyatata93 <soumyatata@dunsterhouse.co.uk>
2024-05-16 21:51:21 +0100

    script added

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Install Node.js and npm
-curl -sL https://rpm.nodesource.com/setup_14.x | bash -
-yum install -y nodejs
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc
+nvm install --lts
 
 # Navigate to the project directory
 cd /var/www/hrsms
 
 # Install project dependencies
-npm install
+# npm install
